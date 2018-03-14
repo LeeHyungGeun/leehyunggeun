f7e0ab2754591cbb8665ce9b08222ae8fecf3292
leehyunggeun <leehyunggeun@navercorp.com>
2018-03-14 15:55:47 +0800

    Add Header

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -1,1 +1,13 @@
 import * as React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                Header
+            </div>
+        );
+    }
+}
+
+export default Header;
