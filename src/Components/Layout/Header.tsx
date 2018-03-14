d387ca9db06cb326d852d037ede19ff2f22379d2
leehyunggeun <leehyunggeun@navercorp.com>
2018-03-14 23:44:23 +0800

    Layout Component, Container

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -1,10 +1,77 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import * as _ from 'lodash';
 
-class Header extends React.Component {
+const Navigation = styled.nav`
+    width: 100%;
+    background-color: #69db7c;
+    & .menu-container {
+        display: flex;
+        justify-content: flex-end;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        list-style-type: none;
+        overflow: hidden;
+        margin-right: 2rem;
+        & .menu-item {
+            padding: 1rem;
+            border-radius: 3px;
+            cursor: pointer;
+            & a {
+                text-decoration: none;
+                color: #fff;
+                outline: none;
+                user-select: none;
+            }
+            &:hover {
+                background: #51cf66;
+            }
+            &.active, :active {
+                background: #51cf66;
+            }
+            & + .menu-item {
+                margin-left: 1rem;
+            }
+        }
+    }
+`;
+
+type Props = {
+    menus: Array<string>;
+    menu: string;
+    onChangeMenu: Function;
+};
+type State = {};
+class Header extends React.Component<Props, State> {
     render() {
+        const {
+            onChangeMenu,
+            menus,
+            menu
+        } = this.props;
         return (
             <div>
-                Header
+                <Navigation>
+                    <ul className="menu-container">
+                        {
+                            _.map(menus, (_menu: string, key: number) => {
+                                return (
+                                    <li 
+                                        className={`menu-item ${_menu === menu ? 'active' : ''}`}
+                                        key={`menu_${key}`}
+                                    >
+                                        <a 
+                                            href="javascript:;"
+                                            onClick={() => { onChangeMenu(_menu); }}
+                                        >
+                                            {_menu}
+                                        </a>
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                </Navigation>
             </div>
         );
     }
--- src/Containers/LayoutContainer.tsx
+++ src/Containers/LayoutContainer.tsx
@@ -1,18 +1,55 @@
 import * as React from 'react';
 import Header from '../Components/Layout/Header';
+import {
+    bindActionCreators
+} from 'redux';
+import {
+    connect
+} from 'react-redux';
+import * as headerActions from '../actions/headerActions';
 
-class Layout extends React.Component {
+type Props = {
+    actions: any;
+    header: any;
+};
+class Layout extends React.Component<Props> {
+    constructor(props: Props) {
+        super(props);
+
+        this.onChangeMenu = this.onChangeMenu.bind(this);
+    }
+    onChangeMenu(menu: string) {
+        const {
+            actions
+        } = this.props;
+        actions.changeMenu(menu);
+    }
     render() {
         const {
+            header,
             children
         } = this.props;
         return (
             <div>
-                <Header />
+                <Header 
+                    menus={header.menus}
+                    menu={header.menu}
+                    onChangeMenu={this.onChangeMenu}
+                />
                 {children}
             </div>
         );
     }
 }
+const mapStateToProps = function(state: any) {
+    return {
+        header: state.header
+    };
+};
+const mapDispatchToProps = function(dispatch: any) {
+    return {
+        actions: bindActionCreators(headerActions, dispatch)
+    };
+};
 
-export default Layout;
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
