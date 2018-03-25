8836cf2af9130cdff9c62c0fd354b519b8738a9d
leehyunggeun <leehyunggeun@navercorp.com>
2018-03-25 11:34:56 +0800

    Add sendMessage API on Server

---
+++ server/apis/contact.ts
@@ -0,0 +1,24 @@
+import mongojs, { ObjectId } from 'mongojs';
+const DB_URL = 'leehyunggeun';
+const COLLECTION_NAME = ['contact'];
+const db = mongojs(DB_URL, COLLECTION_NAME);
+
+function addContact(email = '', body = '', regDate = (new Date()).getTime()) {
+    return new Promise((resolve, reject) => {
+        db.contact.save({
+            email,
+            body,
+            regDate
+        }, (err, doc) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(true);
+            }
+        });
+    });
+}
+
+export default {
+    addContact
+}
