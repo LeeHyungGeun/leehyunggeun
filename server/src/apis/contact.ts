6d601a344e17b898b725875df4cd8985a377d8f6
leehyunggeun <leehyunggeun@navercorp.com>
2018-04-11 22:01:26 +1000

    Contact on Server

--- server/src/apis/contact.ts
+++ server/src/apis/contact.ts
@@ -4,14 +4,12 @@ import mongojs, {
 const DB_URL = 'leehyunggeun';
 const COLLECTION_NAME = ['contact'];
 const db = mongojs(DB_URL, COLLECTION_NAME);
+import Message from '../models/Message';
 
-function addContact(email = '', body = '', regDate = (new Date()).getTime()) {
+function addContact(message: Message) {
+    message.regDate = (new Date()).getTime();
     return new Promise((resolve, reject) => {
-        db.contact.save({
-            email,
-            body,
-            regDate
-        }, (err, doc) => {
+        db.contact.save(message, (err, doc) => {
             if (err) {
                 reject(err);
             } else {
@@ -20,7 +18,19 @@ function addContact(email = '', body = '', regDate = (new Date()).getTime()) {
         });
     });
 }
+function getMessages() {
+    return new Promise((resolve, reject) => {
+        db.contact.find({}, (err, doc) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(doc);
+            }
+        });
+    });
+}
 
 export default {
-    addContact
-}
+    addContact,
+    getMessages
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,33 +5,26 @@ import * as bodyParser from 'body-parser';
 import contact from './apis/contact';
 import * as fs from 'fs';
 // import * as path from 'path';
+import * as _ from 'lodash';
+import Message from './models/Message';
+import {
+    // IpFilter as ipfilter
+} from 'express-ipfilter';
+import acl from './configs/acl';
 
+// app.use(`/admin`, ipfilter(acl.admin, {mode: 'allow'}));
 app.use(express.static('./build'));
 app.use(bodyParser.json());
-
-app.get('/api/getServices', (req, res) => {
-    res.json([
-        {
-            name: 'Home'
-        },
-        {
-            name: 'Contact'
-        },
-        {
-            name: 'Calendar'
-        },
-        {
-            name: 'Message'
-        },
-    ]);
-});
 
 app.post(`/api/sendMessage`, async (req, res) => {
     const {
         email,
         body
     } = req.body;
-    const result = await contact.addContact(email, body);
+    const result = await contact.addContact({
+        email, 
+        body
+    });
     if (result) {
         res.json({
             code: '00',
@@ -44,7 +37,25 @@ app.post(`/api/sendMessage`, async (req, res) => {
         });
     }
 });
+
+app.get(`/api/getMessages`, async (req, res) => {
+    const remoteAddress = req.connection.remoteAddress;
+    if (!_.includes(acl.getMessages, remoteAddress)) {
+        res.json({
+            code: '99',
+            message: 'Invalid IP address'
+        });
+    }
+    const messages: Array<Message> = await contact.getMessages() as Array<Message>;
+    res.json({
+        code: '00',
+        message: 'success',
+        data: messages
+    });
+});
+
 app.get('*', (req, res) => {
+    console.log(req.connection.remoteAddress);
     const url = './build/index.html';
     fs.readFile(url, 'utf8', (err, data) => {
         res.send(data);
