6b9be392b242f4c588a7cca3b83fa08fdf6bfb61
leehyunggeun <leehyunggeun@navercorp.com>
2018-04-11 22:06:45 +1000

    iAuditor Route

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,7 +10,9 @@ import Me from './Containers/MeContainer';
 import Resume from './Containers/ResumeContainer';
 import Contact from './Containers/ContactContainer';
 import ThankYou from './Components/Contact/ThankYou';
+import Admin from './Containers/AdminContainer';
 import variables from './styles/variables';
+import Auditor from './Containers/AuditorContainer';
 
 export default (
     <Switch>
@@ -58,6 +60,20 @@ export default (
                 document.body.style.background = '#f9f9f9';
                 props.menu = 'Contact';
                 return <ThankYou {...props} />;
+            }}
+        />
+        <Route
+            exact={true}
+            path="/Admin"
+            children={<Admin />}
+        />
+        <Route
+            exact={true}
+            path="/iAuditor"
+            render={(props: any) => {
+                document.body.style.background = '#f9f9f9';
+                props.menu = 'iAuditor';
+                return <Auditor {...props} />;
             }}
         />
         <Redirect from="*" to="/" />
